dea4ac63310b0037461b5589c54d1a3b1ef88fad
me <me@robertcurtiss.com>
2021-02-20 16:14:36 -0500

    Init 2

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# https://alien-gadget-305416.ue.r.appspot.com/
 @app.route('/')
 def index():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, Bob says hot potatoes!')  # Press Ctrl+F8 to toggle the breakpoint.
+    return render_template('beer.html')
+        # f'<ul>Hello Greg, Bob says: Bushhhhhhhhhhhhhhh!<ul>'  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
