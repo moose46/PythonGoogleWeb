fdcc9e78cd09bb45592c66a0d9c1abf625f754c9
Robert Curtiss <me@robertcurtiss.com>
2021-05-31 09:03:22 -0400

    Charlotte posted

--- files.py
+++ files.py
@@ -42,6 +42,7 @@ class ProcessDataFiles:
         self.individual_bets['05-09-2021'] = {'Greg': 'Ryan Blaney', 'Bob': 'Kyle Larson'}
         self.individual_bets['05-16-2021'] = {'Greg': 'Ryan Blaney', 'Bob': 'Martin Truex Jr.'}
         self.individual_bets['05-23-2021'] = {'Greg': 'Ryan Blaney', 'Bob': 'Kyle Larson'}
+        self.individual_bets['05-30-2021'] = {'Greg': 'Ryan Blaney', 'Bob': 'Chase Elliott'}
         self.team_bet = defaultdict(list)
 
         self.team_bet['Greg'] = ["Ryan Blaney", "Joey Logano", "Brad Keselowski"]
