0d4f802ae7a8a5ed0a2fe26e866dd0313cee4ef6
me <me@robertcurtiss.com>
2022-10-09 18:28:37 -0400

    Charlotte Roval

--- files.py
+++ files.py
@@ -95,6 +95,7 @@ class ProcessDataFiles:
         self.individual_bets['09-18-2022'] = {'Greg': 'Chase Elliott', 'Bob': 'Christopher Bell'}
         self.individual_bets['09-25-2022'] = {'Greg': 'Ryan Blaney', 'Bob': 'Joey Logano'}
         self.individual_bets['10-02-2022'] = {'Greg': 'Denny Hamlin', 'Bob': 'Ross Chastain'}
+        self.individual_bets['10-09-2022'] = {'Greg': 'Chase Elliott', 'Bob': 'Tyler Reddick'}
         self.team_bet = defaultdict(list)
         bets = [('02-20-2022','Denny Hamlin', 'Brad Keselowski')]
         # self.team_bet['Greg'] = ["Ryan Blaney", "Joey Logano", "Brad Keselowski"]
